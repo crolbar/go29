4b4383c3c567410b34ae38e8e27765bc9c6575d8
crolbar <crolbar@crolbar.com>
2025-02-26 00:31:54 +0200

    add desktop entry

--- flake.nix
+++ flake.nix
@@ -12,12 +12,24 @@
       ];
       buildInputs = with pkgs; [systemd];
     };
-    packages.x86_64-linux.default = pkgs.buildGoModule {
+    packages.x86_64-linux.default = pkgs.buildGoModule rec {
       pname = "go29";
       version = "0.1";
       src = ./.;
       buildInputs = with pkgs; [systemd.dev];
       vendorHash = "sha256-r43FjbKH3g/ZJp43KvvcbPyphZQP/HDmZ11XMS227Bs=";
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p $out
+        dir="$GOPATH/bin"
+        [ -e "$dir" ] && cp -r $dir $out
+
+        mkdir -p $out/share/applications
+        cp $src/${pname}.desktop $out/share/applications
+
+        runHook postInstall
+      '';
     };
   };
 }
